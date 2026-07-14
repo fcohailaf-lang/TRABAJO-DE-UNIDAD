a6b415082cfe83c417788ea911f3b64d21639aec
fcohailaf-lang <fcohailaf@unjbg.edu.pe>
2026-07-14 18:37:45 -0500

    Cabecera de libros.cpp incluido

---
+++ proyecto/libros.h
@@ -0,0 +1,36 @@
+#ifndef LIBROS_H
+#define LIBROS_H
+
+#include "estructuras.h"
+
+int registrarLibro(
+    Libro libros[],
+    int &cantLibros
+);
+
+int listarLibros(
+    Libro libros[],
+    int cantLibros
+);
+
+int buscarLibro(
+    Libro libros[],
+    int cantLibros
+);
+
+int modificarLibro(
+    Libro libros[],
+    int cantLibros
+);
+
+int eliminarLibro(
+    Libro libros[],
+    int &cantLibros
+);
+
+int ordenarLibros(
+    Libro libros[],
+    int cantLibros
+);
+
+#endif
